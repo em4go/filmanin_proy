abe6dbf4e35fe95a473a2789e55a90b00fcde982
em4go <emagodev@gmail.com>
2023-05-16 09:56:45 +0200

    requirements.txt

--- python_files/recommendation.py
+++ python_files/recommendation.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import pandas as pd
-import numpy as np
 import math
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
